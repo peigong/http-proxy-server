ded05e9dbd5bfe29f5c3ccbd8aa3d4226c1033da
周培公 <peigong@foxmail.com>
2014-08-26 18:24:31 +0800

    添加新的DEMO

--- config/default/meta-wikimedia-org-wiki-Special-BannerRandom.js
+++ config/default/meta-wikimedia-org-wiki-Special-BannerRandom.js
@@ -2,10 +2,7 @@ mw.centralNotice.insertBanner( false );
 
 (function(){
     var siteNotice = document.getElementById('siteNotice');
-    if(siteNotice && siteNotice.appendChild){
-        var localAd = document.createElement('div');
-        localAd.style.textAlign = 'left'; 
-        localAd.innerHTML = '<a href="http://www.localad.org.cn" target="_blank"><img src="http://www.localad.org.cn/ads/banner-970x90.jpg" /></a>';
-        siteNotice.appendChild(localAd);
+    if(siteNotice){
+        siteNotice.innerHTML = '<a href="http://www.localad.org.cn" target="_blank"><img src="http://www.localad.org.cn/ads/banner-970x90.jpg" /></a>';
     }
 })();
