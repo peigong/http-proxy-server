35342601ffe9c68af0537f9aa1400d367017a6cd
周培公 <peigong@foxmail.com>
2014-09-14 12:34:17 +0800

    搜狐增加边框

--- config/default/images-sohu.com-bill-s2012-gates-all-sohuad2012v15.js
+++ config/default/images-sohu.com-bill-s2012-gates-all-sohuad2012v15.js
@@ -54,6 +54,7 @@ SohuAd.engine.test = function(beans){
                 if(parent){
                     var ele = document.createElement('div');
                     ele.style.width = '950px';
+                    ele.style.border = '1px solid #cccccc';
                     ele.style.margin = '0px auto 0px auto';
                     ele.style.overflow = 'hidden';
                     ele.innerHTML = '<a href="http://www.localad.org.cn" target="_blank"><img src="http://www.localad.org.cn/ads/banner-970x90.jpg" /></a>';
