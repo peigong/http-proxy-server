3a1565e9267dd652ab698020e8ae4504037e6bcb
周培公 <peigong@foxmail.com>
2014-09-24 12:33:14 +0800

    增加关键词分析中间件

---
+++ server/keywords.js
@@ -0,0 +1,43 @@
+var url = require('url');
+
+module.exports = function(options){
+    var base = '', id_mapping = {}, settings = options.settings;
+    if(options.hasOwnProperty('items')){
+        options.items.map(function(item){
+            id_mapping[item.id] = item;
+        });
+    }
+    return function(req, res, next){
+        var host = req.headers.host, 
+            conf = {}, id = '', key = '', word = '', keywords = {}, query = {};
+
+        if(settings && settings[host]){
+            conf = settings[host];
+            if(conf.key && conf.keywords){
+                key = conf.key;
+                keywords = conf.keywords;
+                query = url.parse(req.url, true).query;
+                if(query && query[key]){
+                    word = query[key];
+                    console.log(word);
+                    if(word && keywords[word]){
+                        id = keywords[word];
+                        console.log(id);
+                        if(id && id_mapping[id]){
+                            item = id_mapping[id];
+                            item.active = true;
+                            var msg = JSON.stringify(item, null, 4);
+                            console.log(msg);
+                            setTimeout(function(){
+                                item.active = false;
+                                var msg = JSON.stringify(item, null, 4);
+                                console.log(msg);
+                            }, 5 * 60 * 1e3);
+                        }
+                    }
+                }
+            }
+        }
+        next();
+    };
+};
--- server/server.js
+++ server/server.js
@@ -3,8 +3,9 @@
     express   = require('express');
 
 var controller = require('./controller.js'),
-    proxy = require('./proxy.js'),
-    filter = require('./filter.js');
+    filter = require('./filter.js'),
+    keywords = require('./keywords.js'),
+    proxy = require('./proxy.js');
 
 var root =  path.resolve(__dirname, '..', '..', '..'),
     configDir = path.resolve(root, 'config');
@@ -14,12 +15,16 @@ var settings = require(path.join(configDir, 'settings.json'));
 var dir = settings.filter;
 var base = path.join(configDir, dir);
 var items = require(path.join(base, 'items.json'));
+var keywordsSettings = require(path.join(configDir, 'keywords.json'));
 if(dir && items){
     var app = express();
     app.use(express.favicon());
     //app.use(express.logger());
     app.use('/ctrl', controller({ base: base, items: items }));
     app.use(filter({ base: base, items: items }));
+    if(keywordsSettings){
+        app.use(keywords({ base: base, items: items, settings: keywordsSettings }));
+    }
     app.use(proxy);
     app.use(function(err, req, res, next){
         if(err){
